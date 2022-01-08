ed1655edeb8d0f21850df443a1c5914283f3ff90
Ernis uulu Renat <rinatovich124@gmail.com>
2022-01-08 22:31:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.querySelector('.input__button-email').onclick = emailClick ;
+
+function emailClick() {
+    console.log("great");
+
+
+//get input ;
+     let a = document.querySelector('.input__text').value ;
+    console.log(a);
+     
+}
+
+document.querySelector('.input__button-password').onclick = passwordClick;
+
+function passwordClick() {
+    console.log('great')
+
+
+//get input ;
+
+    let b = document.querySelector('.input__password').value;
+    console.log(b);
+}
+
+console.log('salam');
+
+
+
+
+    
